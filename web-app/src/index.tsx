2f22ef6a6a7f08c34a8083acd41504a46ae2a741
vincentDubresson <vincent.dubresson@orange.fr>
2022-12-20 13:38:55 +0100

    cleanup project - Switch index.scss with index.css before sass install

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-import "./index.scss";
+import "./index.css";
 import App from "./App/App";
 import reportWebVitals from "./reportWebVitals";
 
