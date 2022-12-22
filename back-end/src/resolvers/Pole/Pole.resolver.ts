1acabced79a0104c2b7dd59e8c07b13081c1078f
Anthony Gouton <gouton.anthony@live.fr>
2022-12-22 18:54:38 +0100

    Implementation of methods for creating, updating and deleting poles

---
+++ back-end/src/resolvers/Pole/Pole.resolver.ts
@@ -0,0 +1,35 @@
+import { Arg, Args, Ctx, Mutation, Query, Resolver } from "type-graphql";
+
+import Pole from "../../models/Pole/Pole.entity";
+import PoleRepository from "../../models/Pole/Pole.repository";
+import {
+  CreatePoleArgs,
+  UpdatePoleArgs,
+} from "./Pole.input";
+
+@Resolver(Pole)
+export default class PoleResolver {
+  @Query(() => [Pole])
+  poles(): Promise<Pole[]> {
+    return PoleRepository.getPoles();
+  }
+
+  @Mutation(() => Pole)
+  createPole(
+    @Args() { name, address, zip_code, city, email }: CreatePoleArgs
+  ): Promise<Pole> {
+    return PoleRepository.createPole(name, address, zip_code, city, email);
+  }
+
+  @Mutation(() => Pole)
+  updatePoleName(
+    @Args() { id, name, address, zip_code, city, email }: UpdatePoleArgs
+  ): Promise<Pole> {
+    return PoleRepository.updatePole(id, name, address, zip_code, city, email);
+  }
+
+  @Mutation(() => Pole)
+  deletePole(@Arg("id") id: string): Promise<Pole> {
+    return PoleRepository.deletePole(id);
+  }
+}
