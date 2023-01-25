172f054a92139de23fea7e2f393adc4d09bfbc17
Anthony Gouton <gouton.anthony@live.fr>
2023-01-25 14:56:41 +0100

    Add Validation for Pole and Restaurant

--- back-end/src/resolvers/Pole/Pole.input.ts
+++ back-end/src/resolvers/Pole/Pole.input.ts
@@ -1,24 +1,47 @@
-import { IsUUID, MinLength } from "class-validator";
+import {
+  IsUUID,
+  MinLength,
+  MaxLength,
+  IsString,
+  Matches,
+  IsEmail,
+} from "class-validator";
 import { ArgsType, Field, ID } from "type-graphql";
 
 @ArgsType()
 class CreatePoleArgs {
   @Field()
-  @MinLength(1, {
-    message: "Le nom doit faire au moins un caractère de long.",
+  @IsString({ message: "Le nom doit être une chaine de caractère." })
+  @MinLength(1, { message: "Le nom doit faire au moins un caractère de long." })
+  @MaxLength(255, {
+    message: "Le nom doit faire au maximum 255 caractères de long.",
   })
   name: string;
 
   @Field()
+  @IsString({ message: "L'adresse doit être une chaine de caractère." })
+  @MaxLength(255, {
+    message: "L'adresse doit faire au maximum 255 caractères de long.",
+  })
   address: string;
 
   @Field()
+  @IsString({ message: "Le code postal doit être une chaine de caractère." })
+  @Matches(/^[0-9]{5}$/, {
+    message: "Le code postal n'est pas un code postal valide.",
+  })
   zipCode: string;
 
   @Field()
+  @IsString({ message: "La ville doit être une chaine de caractère." })
+  @MaxLength(255, {
+    message: "La ville doit faire au maximum 255 caractères de long.",
+  })
   city: string;
 
   @Field()
+  @IsString({ message: "L'adresse email doit être une chaine de caractère." })
+  @IsEmail({ message: "L'adresse email n'est pas une adresse email valide." })
   email: string;
 }
 
