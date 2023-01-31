6a0e3d1a6135fdf964cf95e8f359835330d07690
vincentDubresson <vincent.dubresson@orange.fr>
2023-01-31 18:32:11 +0100

    Add signout in dashboard component + Create a new Protected component for restrictions.

--- web-app/src/pages/Dashboard/Dashboard.tsx
+++ web-app/src/pages/Dashboard/Dashboard.tsx
@@ -1,19 +1,68 @@
 import React from "react";
-import './Dashboard.scss';
+import "./Dashboard.scss";
 import logoLightBig from "../../assets/logos/r-ticket-light-big.png";
+import { gql, useMutation } from "@apollo/client";
+import { SignOutMutation, SignOutMutationVariables } from "../../gql/graphql";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { HOME_PATH } from "../paths";
 
-const Dashboard = ({ userData }: { userData: any }) => {
-  console.log(userData);
+const SIGN_OUT = gql`
+  mutation SignOut($signOutId: String!) {
+    signOut(id: $signOutId) {
+      id
+    }
+  }
+`;
+
+const Dashboard = ({
+  userData,
+  onSuccess,
+}: {
+  userData: any;
+  onSuccess: () => {};
+}) => {
+  const navigate = useNavigate();
+
+  const [signOut] = useMutation<SignOutMutation, SignOutMutationVariables>(
+    SIGN_OUT,
+    {
+      onCompleted: () => {
+        toast.success("Vous êtes déconnecté.");
+        onSuccess();
+        //TODO: Voir pour supprimer les cookies dans le navigateur.
+        navigate(HOME_PATH);
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    }
+  );
+
+  const handleDisconnect = async (user: any) => {
+    signOut({
+      variables: {
+        signOutId: user.id,
+      },
+    });
+  };
 
   return (
-      <div className="DashboardMain">
-        <div className="DashboardContent">
-          <img className="DashboardLogo" src={logoLightBig} alt="Logo R'Ticket" />
-          <p className="DashboardText">Page Under Construction...</p>
-          <p>Connecté avec l'adresse email : {userData.email}</p>
-          <button className="DashboardButton">Déconnexion</button>
-        </div>
+    <div className="DashboardMain">
+      <div className="DashboardContent">
+        <img className="DashboardLogo" src={logoLightBig} alt="Logo R'Ticket" />
+        <p className="DashboardText">Page Under Construction...</p>
+        <p>Connecté avec l'adresse email : {userData.email}</p>
+        <button
+          className="DashboardButton"
+          onClick={() => {
+            handleDisconnect(userData);
+          }}
+        >
+          Déconnexion
+        </button>
       </div>
+    </div>
   );
 };
 
--- web-app/src/pages/NotFound/NotFound.tsx
+++ web-app/src/pages/NotFound/NotFound.tsx
@@ -1,15 +1,25 @@
 import "./NotFound.scss";
 
 import logoLightBig from "../../assets/logos/r-ticket-light-big.png";
+import { useNavigate } from "react-router-dom";
+import { HOME_PATH } from "../paths";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate(HOME_PATH);
+  };
+
   return (
     <div className="NotFoundMain">
       <div className="NotFoundContent">
         <img className="NotFoundLogo" src={logoLightBig} alt="Logo R'Ticket" />
         <p className="NotFound404Text">404</p>
         <p className="NotFoundText">La page recherchée n'existe pas.</p>
-        <button className="NotFoundButton">Retour à l'accueil</button>
+        <button className="NotFoundButton" onClick={handleRedirect}>
+          Retour à l'accueil
+        </button>
       </div>
     </div>
   );
