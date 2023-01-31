4f713eb0536b313df544cdf2236ed96a00f9ad93
vincentDubresson <vincent.dubresson@orange.fr>
2023-01-31 14:32:32 +0100

    Add Home page with access button : if not auth : the user goes to signin page, else : he goes directly in the dashboard

---
+++ web-app/src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dashboard = ({userData}: {userData: any}) => {
+  console.log(userData);
+
+  return (
+    <div>
+      <p>dashboard !</p>
+      <button>Signout</button>
+    </div>
+  );
+}
+
+export default Dashboard;
