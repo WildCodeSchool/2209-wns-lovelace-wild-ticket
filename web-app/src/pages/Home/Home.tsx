4dfdabed2abab1bf36db19bc7b1b867bb642e031
vincentDubresson <vincent.dubresson@orange.fr>
2023-01-31 15:10:46 +0100

    Add a 404 page

--- web-app/src/pages/Home/Home.tsx
+++ web-app/src/pages/Home/Home.tsx
@@ -1,9 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { DASHBOARD_HOME, SIGN_IN_PATH } from "../paths";
-import './Home.scss';
+import "./Home.scss";
+import logoLightBig from "../../assets/logos/r-ticket-light-big.png";
 
-const Home = ({isUserAuth }: { isUserAuth: boolean }) => {
-
+const Home = ({ isUserAuth }: { isUserAuth: boolean }) => {
   const navigate = useNavigate();
 
   console.log(isUserAuth);
@@ -14,13 +14,16 @@ const Home = ({isUserAuth }: { isUserAuth: boolean }) => {
     } else {
       navigate(SIGN_IN_PATH);
     }
-  }
+  };
 
   //TODO: FAIRE LA PAGE D'ACCUEIL (PRESENTATION EQUIPE + BOUTON ACCES DASHBOARD)
   return (
-    <div className='HomePage'>
-      <div className='HomePageButtonContainer'>
-        <button onClick={handleButton}>Accès au dashboard R'Ticket</button>
+    <div className="HomePage">
+      <div className="HomePageButtonContainer">
+        <img className="NotFoundLogo" src={logoLightBig} alt="Logo R'Ticket" />
+        <button className="HomePageButton" onClick={handleButton}>
+          Accès au dashboard R'Ticket
+        </button>
       </div>
     </div>
   );
