724c958f824ce26c49b92e32182651ded0b64ef0
vincentDubresson <vincent.dubresson@orange.fr>
2023-01-31 15:24:44 +0100

    Modify Dashboard by adding construction page

--- web-app/src/pages/Dashboard/Dashboard.tsx
+++ web-app/src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import './Dashboard.scss';
+import logoLightBig from "../../assets/logos/r-ticket-light-big.png";
 
 const Dashboard = ({ userData }: { userData: any }) => {
   console.log(userData);
 
   return (
-    <div>
-      <p>dashboard !</p>
-      <button>Signout</button>
-    </div>
+      <div className="DashboardMain">
+        <div className="DashboardContent">
+          <img className="DashboardLogo" src={logoLightBig} alt="Logo R'Ticket" />
+          <p className="DashboardText">Page Under Construction...</p>
+          <p>Connecté avec l'adresse email : {userData.email}</p>
+          <button className="DashboardButton">Déconnexion</button>
+        </div>
+      </div>
   );
 };
 
