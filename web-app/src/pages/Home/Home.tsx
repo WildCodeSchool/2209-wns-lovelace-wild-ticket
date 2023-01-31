884698a77e711c92dff947c6911bbac564d0c7d8
vincentDubresson <vincent.dubresson@orange.fr>
2023-01-31 14:32:32 +0100

    Add Home page with access button : if not auth : the user goes to signin page, else : he goes directly in the dashboard

--- web-app/src/pages/Home/Home.tsx
+++ web-app/src/pages/Home/Home.tsx
@@ -1,13 +1,28 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { DASHBOARD_HOME, SIGN_IN_PATH } from "../paths";
+import './Home.scss';
 
-const Home = () => {
+const Home = ({isUserAuth }: { isUserAuth: boolean }) => {
 
+  const navigate = useNavigate();
+
+  console.log(isUserAuth);
+
+  const handleButton = (): void => {
+    if (isUserAuth) {
+      navigate(DASHBOARD_HOME);
+    } else {
+      navigate(SIGN_IN_PATH);
+    }
+  }
+
+  //TODO: FAIRE LA PAGE D'ACCUEIL (PRESENTATION EQUIPE + BOUTON ACCES DASHBOARD)
   return (
-    <>
-      <h1>Home Page</h1>
-      <br />
-      <br />
-    </>
+    <div className='HomePage'>
+      <div className='HomePageButtonContainer'>
+        <button onClick={handleButton}>Accès au dashboard R'Ticket</button>
+      </div>
+    </div>
   );
 };
 
