ae685978135213a4ebf7d0c1830d5cf97d959723
vincentDubresson <vincent.dubresson@orange.fr>
2023-02-01 15:53:36 +0100

    Add the possibility to change the size of the svg logo

--- web-app/src/pages/Home/Home.tsx
+++ web-app/src/pages/Home/Home.tsx
@@ -3,11 +3,12 @@ import { DASHBOARD_HOME, SIGN_IN_PATH } from "../paths";
 import "./Home.scss";
 import { useContext } from "react";
 import { UserContext } from "../../context/UserContext";
-import SVGLogoBig from "../../components/SVGLogoBig/SVGLogoBig";
+import SVGLogo from "../../components/SVGLogo/SVGLogo";
 
 const Home = () => {
   const userContext = useContext(UserContext);
   const navigate = useNavigate();
+  const logoSize = "500";
 
   const handleButton = (): void => {
     if (userContext?.isAuthenticated) {
@@ -21,7 +22,7 @@ const Home = () => {
   return (
     <div className="HomePage">
       <div className="HomePageButtonContainer">
-        <SVGLogoBig />
+      <SVGLogo logoWidth={logoSize} logoHeight={logoSize} />
         <button className="HomePageButton" onClick={handleButton}>
           Accès au dashboard R'Ticket
         </button>
