c5671b6130b9f6e53f4096ab3dcf93a995462573
vincentDubresson <vincent.dubresson@orange.fr>
2023-02-01 15:37:44 +0100

    Integration of a sidebar component when path includes "dashboard"

--- web-app/src/App/App.tsx
+++ web-app/src/App/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 import Home from "../pages/Home/Home";
@@ -9,24 +9,30 @@ import SignIn from "../pages/SignIn/SignIn";
 import Protected from "../components/Protected/Protected";
 
 import { DASHBOARD_HOME, HOME_PATH, SIGN_IN_PATH } from "../pages/paths";
+import SideBar from "../components/Sidebar/Sidebar";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <main>
-        <Routes>
-          <Route path={HOME_PATH} element={<Home />} />
-          <Route path={SIGN_IN_PATH} element={<SignIn />} />
-          <Route
-            path={DASHBOARD_HOME}
-            element={
-              <Protected>
-                <Dashboard />
-              </Protected>
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        {location.pathname.includes(DASHBOARD_HOME) && <SideBar />}
+        <>
+          <Routes>
+            <Route path={HOME_PATH} element={<Home />} />
+            <Route path={SIGN_IN_PATH} element={<SignIn />} />
+            <Route
+              path={DASHBOARD_HOME}
+              element={
+                <Protected>
+                  <Dashboard />
+                </Protected>
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </>
       </main>
       <ToastContainer />
     </>
