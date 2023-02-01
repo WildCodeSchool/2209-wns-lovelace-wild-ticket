f3073aa586918a75c56c3db535b18dc939558d8e
vincentDubresson <vincent.dubresson@orange.fr>
2023-02-01 23:25:03 +0100

    Add paths and first part of integration of all dashborad components

--- web-app/src/pages/paths.ts
+++ web-app/src/pages/paths.ts
@@ -2,4 +2,11 @@ export const HOME_PATH = "/";
 export const SIGN_UP_PATH = "/sign-up";
 export const SIGN_IN_PATH = "/sign-in";
 
-export const DASHBOARD_HOME = "/dashboard";
+export const DASHBOARD_HOME = "/dashboard/";
+export const DASHBOARD_RESTAURANT = "restaurants";
+export const DASHBOARD_POLE = "poles";
+export const DASHBOARD_USER = "users";
+export const DASHBOARD_TICKET = "tickets";
+export const DASHBOARD_TABLE = "tables";
+export const DASHBOARD_STATS = "stats";
+export const DASHBOARD_OPTIONS = "options";
