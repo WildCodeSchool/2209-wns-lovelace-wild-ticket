b386ca22d78ddb0fc3e02db6aa09ebb5c912ffc0
vincentDubresson <vincent.dubresson@orange.fr>
2023-02-01 11:26:01 +0100

    Add a user context to prevent props utilization

--- web-app/src/App/App.tsx
+++ web-app/src/App/App.tsx
@@ -1,64 +1,27 @@
-import React, { useState } from "react";
-import { gql, useQuery } from "@apollo/client";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { MyProfileQuery } from "../gql/graphql";
 
 import Home from "../pages/Home/Home";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import NotFound from "../pages/NotFound/NotFound";
-import { DASHBOARD_HOME, HOME_PATH, SIGN_IN_PATH } from "../pages/paths";
 import SignIn from "../pages/SignIn/SignIn";
 import Protected from "../components/Protected/Protected";
 
-const MY_PROFILE = gql`
-  query MyProfile {
-    myProfile {
-      id
-      email
-      role
-      poles {
-        id
-        name
-      }
-      restaurant {
-        id
-        name
-      }
-    }
-  }
-`;
+import { DASHBOARD_HOME, HOME_PATH, SIGN_IN_PATH } from "../pages/paths";
 
 function App() {
-  const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
-  const [user, setUser] = useState({});
-
-  const { refetch } = useQuery<MyProfileQuery>(MY_PROFILE, {
-    onCompleted: (data) => {
-      if (data.myProfile) {
-        setIsUserAuthenticated(true);
-        setUser(data.myProfile);
-      }
-    },
-    onError: () => {
-      setIsUserAuthenticated(false);
-    },
-  });
-
   return (
     <>
       <main>
         <Routes>
-          <Route
-            path={HOME_PATH}
-            element={<Home isUserAuth={isUserAuthenticated} />}
-          />
-          <Route path={SIGN_IN_PATH} element={<SignIn onSuccess={refetch} />} />
+          <Route path={HOME_PATH} element={<Home />} />
+          <Route path={SIGN_IN_PATH} element={<SignIn />} />
           <Route
             path={DASHBOARD_HOME}
             element={
-              <Protected userData={user}>
-                <Dashboard userData={user} onSuccess={refetch} />
+              <Protected>
+                <Dashboard />
               </Protected>
             }
           />
