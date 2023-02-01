c6d6075d1c5f705484c530d0bd7cdfcc760eb31a
Estelle <estelle9.dev@gmail.com>
2023-02-01 19:50:11 +0100

    created submit ticket updated

--- mobile-app/screens/CreateTicketScreen.tsx
+++ mobile-app/screens/CreateTicketScreen.tsx
@@ -1,8 +1,19 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import React from "react";
 import { RootStackScreenProps } from "../types";
 
 const CreateTicketScreen = ({ navigation }: RootStackScreenProps<"Ticket">) => {
+  const [firstName, onChangeFirstName] = React.useState("");
+  const [email, onChangeEmail] = React.useState("");
+  const [number, onChangeNumber] = React.useState("");
+
   return (
     <View>
       <Button
@@ -11,9 +22,22 @@ const CreateTicketScreen = ({ navigation }: RootStackScreenProps<"Ticket">) => {
       />
       <View>
         <Text>Infos restaurant</Text>
+        <Text>Restaurant :</Text>
+        <Text>Nombre de couverts :</Text>
+        <Text>Temps d’attente estimé :</Text>
       </View>
       <View>
         <Text>Formulaire</Text>
+        <SafeAreaView>
+          <TextInput style={styles.input} onChangeText={onChangeFirstName} value={firstName} placeholder="Name"/>
+          <TextInput style={styles.input} onChangeText={onChangeEmail} value={email} placeholder="Email" keyboardType="email-address" />
+          <TextInput style={styles.input}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="Phone number"
+            keyboardType="phone-pad"
+          />
+        </SafeAreaView>
       </View>
     </View>
   );
@@ -21,4 +45,11 @@ const CreateTicketScreen = ({ navigation }: RootStackScreenProps<"Ticket">) => {
 
 export default CreateTicketScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
