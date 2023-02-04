f190248fc13163e6ed3ec0bd8e90f60eb567e7d4
vincentDubresson <vincent.dubresson@orange.fr>
2023-02-04 18:55:28 +0100

    First part of integration of the ticket list in the dashboard

---
+++ web-app/src/components/Dashboard/DashboardListTab/DashboardListTab.tsx
@@ -0,0 +1,228 @@
+import "./DashboardListTab.scss";
+
+export default function DashboardListTab() {
+  return (
+    <table id="ministres">
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>N° Ticket</th>
+          <th>Couverts</th>
+          <th>Temps d'attente</th>
+          <th>Notification</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Nom</td>
+          <td>N° Ticket</td>
+          <td>Couverts</td>
+          <td>Temps d'attente</td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
--- web-app/src/pages/Dashboard/DashboardTicket/DashboardTicket.tsx
+++ web-app/src/pages/Dashboard/DashboardTicket/DashboardTicket.tsx
@@ -1,25 +1,23 @@
-import { useContext } from "react";
-import "../DashboardTemp.scss";
-import { UserContext } from "../../../context/UserContext";
-import SVGLogo from "../../../components/SVG/SVGLogo/SVGLogo";
-import { BIG_LOGO_DASHBOARD_SIZE } from "../../../constants/Constants";
+// TODO: Quand toutes les pages seront créées, modifier l'import sur toutes les autres pages dashboard comme suit.
+import DashboardListTab from "../../../components/Dashboard/DashboardListTab/DashboardListTab";
+import "../DashboardMain.scss";
 
 const DashboardTicket = () => {
-  const userContext = useContext(UserContext);
-
   return (
-    <div className="DashboardMain">
-      <div className="DashboardContent">
-        <SVGLogo
-          logoWidth={BIG_LOGO_DASHBOARD_SIZE}
-          logoHeight={BIG_LOGO_DASHBOARD_SIZE}
-          logoFill={userContext?.userSVGColorScheme}
-        />
-        <h1>DASHBOARD TICKET</h1>
-        <p className="DashboardText">Page Under Construction...</p>
-        <p>Connecté avec l'adresse email : {userContext?.userData.email}</p>
-      </div>
-    </div>
+    <section className="DashboardMainSection">
+      {/* TODO: Le header sera utilisé pour les filtres, bouton d'ajout etc. (cf. maquette) */}
+      <header className="DashboardMainHeader">
+        <h1>FILTRES ET BOUTONS</h1>
+        <p className="DashboardText">Under Construction...</p>
+      </header>
+      <main className="DashboardMainList">
+        <DashboardListTab />
+      </main>
+      <footer className="DashboardMainFooter">
+        <h1>PAGINATION</h1>
+        <p className="DashboardText">Under Construction...</p>
+      </footer>
+    </section>
   );
 };
 
