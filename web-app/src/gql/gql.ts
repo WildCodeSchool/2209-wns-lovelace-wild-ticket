decaf34a7c0baa5930dfbce3215fe885f51b8a37
vincentDubresson <vincent.dubresson@orange.fr>
2023-03-01 10:51:08 +0100

    Finalize ticket tab list 6 Add a new spinner + Refacto + CSS

--- web-app/src/gql/gql.ts
+++ web-app/src/gql/gql.ts
@@ -1,26 +1,55 @@
 /* eslint-disable */
-import * as types from './graphql';
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import * as types from "./graphql";
+import { TypedDocumentNode as DocumentNode } from "@graphql-typed-document-node/core";
 
 const documents = {
-    "\n  mutation SignOut($signOutId: String!) {\n    signOut(id: $signOutId) {\n      id\n    }\n  }\n": types.SignOutDocument,
-    "\n  query MyProfile {\n    myProfile {\n      id\n      email\n      role\n      poles {\n        id\n        name\n      }\n      restaurant {\n        id\n        name\n      }\n    }\n  }\n": types.MyProfileDocument,
-    "\n  query TicketsByRestaurant($ticketsByRestaurantId: String!) {\n    TicketsByRestaurant(id: $ticketsByRestaurantId) {\n      id\n      number\n      name\n      seats\n      email\n      phoneNumber\n      createdAt\n      deliveredAt\n      placedAt\n      closedAt\n      table {\n        id\n        number\n      }\n    }\n  }\n": types.TicketsByRestaurantDocument,
-    "\n  query TablesByRestaurant($tablesByRestaurantId: String!) {\n    TablesByRestaurant(id: $tablesByRestaurantId) {\n      id\n      number\n      capacity\n    }\n  }\n": types.TablesByRestaurantDocument,
-    "\nmutation UpdateClosedAt($updateClosedAtId: String!) {\n  updateClosedAt(id: $updateClosedAtId) {\n    id\n    closedAt\n  }\n}\n": types.UpdateClosedAtDocument,
-    "\n  mutation SignIn($email: String!, $password: String!, $rememberMe: Boolean!) {\n    signIn(email: $email, password: $password, rememberMe: $rememberMe) {\n      id\n      email\n    }\n  }\n": types.SignInDocument,
+  "\n  mutation SignOut($signOutId: String!) {\n    signOut(id: $signOutId) {\n      id\n    }\n  }\n":
+    types.SignOutDocument,
+  "\n  query MyProfile {\n    myProfile {\n      id\n      email\n      role\n      poles {\n        id\n        name\n      }\n      restaurant {\n        id\n        name\n      }\n    }\n  }\n":
+    types.MyProfileDocument,
+  "\n  query TicketsByRestaurant($ticketsByRestaurantId: String!) {\n    TicketsByRestaurant(id: $ticketsByRestaurantId) {\n      id\n      number\n      name\n      seats\n      email\n      phoneNumber\n      createdAt\n      deliveredAt\n      placedAt\n      closedAt\n      table {\n        id\n        number\n      }\n    }\n  }\n":
+    types.TicketsByRestaurantDocument,
+  "\n  query TablesByRestaurant($tablesByRestaurantId: String!) {\n    TablesByRestaurant(id: $tablesByRestaurantId) {\n      id\n      number\n      capacity\n    }\n  }\n":
+    types.TablesByRestaurantDocument,
+  "\n  mutation UpdateDeliveredAt($updateDeliveredAtId: ID!, $table: String!) {\n    updateDeliveredAt(id: $updateDeliveredAtId, table: $table) {\n      id\n    }\n  }\n":
+    types.UpdateDeliveredAtDocument,
+  "\n  mutation UpdatePlacedAt($updatePlacedAtId: String!) {\n    updatePlacedAt(id: $updatePlacedAtId) {\n      id\n      deliveredAt\n      placedAt\n      closedAt\n    }\n  }\n":
+    types.UpdatePlacedAtDocument,
+  "\n  mutation UpdateClosedAt($updateClosedAtId: String!) {\n    updateClosedAt(id: $updateClosedAtId) {\n      id\n      closedAt\n    }\n  }\n":
+    types.UpdateClosedAtDocument,
+  "\n  mutation SignIn($email: String!, $password: String!, $rememberMe: Boolean!) {\n    signIn(email: $email, password: $password, rememberMe: $rememberMe) {\n      id\n      email\n    }\n  }\n":
+    types.SignInDocument,
 };
 
-export function graphql(source: "\n  mutation SignOut($signOutId: String!) {\n    signOut(id: $signOutId) {\n      id\n    }\n  }\n"): (typeof documents)["\n  mutation SignOut($signOutId: String!) {\n    signOut(id: $signOutId) {\n      id\n    }\n  }\n"];
-export function graphql(source: "\n  query MyProfile {\n    myProfile {\n      id\n      email\n      role\n      poles {\n        id\n        name\n      }\n      restaurant {\n        id\n        name\n      }\n    }\n  }\n"): (typeof documents)["\n  query MyProfile {\n    myProfile {\n      id\n      email\n      role\n      poles {\n        id\n        name\n      }\n      restaurant {\n        id\n        name\n      }\n    }\n  }\n"];
-export function graphql(source: "\n  query TicketsByRestaurant($ticketsByRestaurantId: String!) {\n    TicketsByRestaurant(id: $ticketsByRestaurantId) {\n      id\n      number\n      name\n      seats\n      email\n      phoneNumber\n      createdAt\n      deliveredAt\n      placedAt\n      closedAt\n      table {\n        id\n        number\n      }\n    }\n  }\n"): (typeof documents)["\n  query TicketsByRestaurant($ticketsByRestaurantId: String!) {\n    TicketsByRestaurant(id: $ticketsByRestaurantId) {\n      id\n      number\n      name\n      seats\n      email\n      phoneNumber\n      createdAt\n      deliveredAt\n      placedAt\n      closedAt\n      table {\n        id\n        number\n      }\n    }\n  }\n"];
-export function graphql(source: "\n  query TablesByRestaurant($tablesByRestaurantId: String!) {\n    TablesByRestaurant(id: $tablesByRestaurantId) {\n      id\n      number\n      capacity\n    }\n  }\n"): (typeof documents)["\n  query TablesByRestaurant($tablesByRestaurantId: String!) {\n    TablesByRestaurant(id: $tablesByRestaurantId) {\n      id\n      number\n      capacity\n    }\n  }\n"];
-export function graphql(source: "\nmutation UpdateClosedAt($updateClosedAtId: String!) {\n  updateClosedAt(id: $updateClosedAtId) {\n    id\n    closedAt\n  }\n}\n"): (typeof documents)["\nmutation UpdateClosedAt($updateClosedAtId: String!) {\n  updateClosedAt(id: $updateClosedAtId) {\n    id\n    closedAt\n  }\n}\n"];
-export function graphql(source: "\n  mutation SignIn($email: String!, $password: String!, $rememberMe: Boolean!) {\n    signIn(email: $email, password: $password, rememberMe: $rememberMe) {\n      id\n      email\n    }\n  }\n"): (typeof documents)["\n  mutation SignIn($email: String!, $password: String!, $rememberMe: Boolean!) {\n    signIn(email: $email, password: $password, rememberMe: $rememberMe) {\n      id\n      email\n    }\n  }\n"];
+export function graphql(
+  source: "\n  mutation SignOut($signOutId: String!) {\n    signOut(id: $signOutId) {\n      id\n    }\n  }\n"
+): typeof documents["\n  mutation SignOut($signOutId: String!) {\n    signOut(id: $signOutId) {\n      id\n    }\n  }\n"];
+export function graphql(
+  source: "\n  query MyProfile {\n    myProfile {\n      id\n      email\n      role\n      poles {\n        id\n        name\n      }\n      restaurant {\n        id\n        name\n      }\n    }\n  }\n"
+): typeof documents["\n  query MyProfile {\n    myProfile {\n      id\n      email\n      role\n      poles {\n        id\n        name\n      }\n      restaurant {\n        id\n        name\n      }\n    }\n  }\n"];
+export function graphql(
+  source: "\n  query TicketsByRestaurant($ticketsByRestaurantId: String!) {\n    TicketsByRestaurant(id: $ticketsByRestaurantId) {\n      id\n      number\n      name\n      seats\n      email\n      phoneNumber\n      createdAt\n      deliveredAt\n      placedAt\n      closedAt\n      table {\n        id\n        number\n      }\n    }\n  }\n"
+): typeof documents["\n  query TicketsByRestaurant($ticketsByRestaurantId: String!) {\n    TicketsByRestaurant(id: $ticketsByRestaurantId) {\n      id\n      number\n      name\n      seats\n      email\n      phoneNumber\n      createdAt\n      deliveredAt\n      placedAt\n      closedAt\n      table {\n        id\n        number\n      }\n    }\n  }\n"];
+export function graphql(
+  source: "\n  query TablesByRestaurant($tablesByRestaurantId: String!) {\n    TablesByRestaurant(id: $tablesByRestaurantId) {\n      id\n      number\n      capacity\n    }\n  }\n"
+): typeof documents["\n  query TablesByRestaurant($tablesByRestaurantId: String!) {\n    TablesByRestaurant(id: $tablesByRestaurantId) {\n      id\n      number\n      capacity\n    }\n  }\n"];
+export function graphql(
+  source: "\n  mutation UpdateDeliveredAt($updateDeliveredAtId: ID!, $table: String!) {\n    updateDeliveredAt(id: $updateDeliveredAtId, table: $table) {\n      id\n    }\n  }\n"
+): typeof documents["\n  mutation UpdateDeliveredAt($updateDeliveredAtId: ID!, $table: String!) {\n    updateDeliveredAt(id: $updateDeliveredAtId, table: $table) {\n      id\n    }\n  }\n"];
+export function graphql(
+  source: "\n  mutation UpdatePlacedAt($updatePlacedAtId: String!) {\n    updatePlacedAt(id: $updatePlacedAtId) {\n      id\n      deliveredAt\n      placedAt\n      closedAt\n    }\n  }\n"
+): typeof documents["\n  mutation UpdatePlacedAt($updatePlacedAtId: String!) {\n    updatePlacedAt(id: $updatePlacedAtId) {\n      id\n      deliveredAt\n      placedAt\n      closedAt\n    }\n  }\n"];
+export function graphql(
+  source: "\n  mutation UpdateClosedAt($updateClosedAtId: String!) {\n    updateClosedAt(id: $updateClosedAtId) {\n      id\n      closedAt\n    }\n  }\n"
+): typeof documents["\n  mutation UpdateClosedAt($updateClosedAtId: String!) {\n    updateClosedAt(id: $updateClosedAtId) {\n      id\n      closedAt\n    }\n  }\n"];
+export function graphql(
+  source: "\n  mutation SignIn($email: String!, $password: String!, $rememberMe: Boolean!) {\n    signIn(email: $email, password: $password, rememberMe: $rememberMe) {\n      id\n      email\n    }\n  }\n"
+): typeof documents["\n  mutation SignIn($email: String!, $password: String!, $rememberMe: Boolean!) {\n    signIn(email: $email, password: $password, rememberMe: $rememberMe) {\n      id\n      email\n    }\n  }\n"];
 
 export function graphql(source: string): unknown;
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> =
+  TDocumentNode extends DocumentNode<infer TType, any> ? TType : never;
