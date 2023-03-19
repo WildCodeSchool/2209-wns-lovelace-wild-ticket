48788bd09f4b70d038354a3895bb4e3f35ce0589
vincentDubresson <vincent.dubresson@orange.fr>
2023-03-19 19:47:45 +0100

    Revert "Add Background on mobile-app Home Page"
    This reverts commit 4d75b67742ae34887631c96d73e397d28415bc56.

--- mobile-app/screens/HomeScreen.tsx
+++ mobile-app/screens/HomeScreen.tsx
@@ -1,62 +1,38 @@
-import {
-  ImageBackground,
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { Button, Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { RootStackScreenProps } from "../types";
 
-const background = {
-  uri: "https://i.ibb.co/j3bbZdt/r-ticket-background.png",
-};
-
 const HomeScreen = ({ navigation }: RootStackScreenProps<"Home">) => {
   return (
-    <ImageBackground
-      source={background}
-      resizeMode="stretch"
-      style={styles.background}
-    >
-      <View style={styles.container}>
-        <Pressable
-          onPress={() => navigation.navigate("Select")}
-          style={styles.button}
-        >
-          <Text style={styles.text}>Appuyez pour continuer</Text>
-        </Pressable>
-      </View>
-    </ImageBackground>
+    <View style={styles.container}>
+      <Pressable
+        onPress={() => navigation.navigate("Select")}
+        style={styles.button}
+      >
+        <Text style={styles.text}>Appuyez pour continuer</Text>
+      </Pressable>
+    </View>
   );
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
-  background: {
-    flex: 1,
-    justifyContent: "center",
-  },
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
   button: {
-    position: "absolute",
-    bottom: 160,
-    right: 260,
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 10,
+    borderRadius: 4,
     backgroundColor: "none",
-    borderWidth: 4,
-    borderColor: "#e4cf63",
-    height: 90,
-    width: 300,
+    borderWidth: 3,
+    height: 60,
+    width: 225,
   },
   text: {
-    fontSize: 24,
+    fontSize: 16,
     lineHeight: 21,
     fontWeight: "bold",
     letterSpacing: 0.25,
-    color: "#e4cf63",
+    color: "black",
   },
 });
