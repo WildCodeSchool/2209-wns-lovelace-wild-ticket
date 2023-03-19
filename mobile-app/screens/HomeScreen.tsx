4d75b67742ae34887631c96d73e397d28415bc56
vincentDubresson <vincent.dubresson@orange.fr>
2023-03-19 18:19:14 +0100

    Add Background on mobile-app Home Page

--- mobile-app/screens/HomeScreen.tsx
+++ mobile-app/screens/HomeScreen.tsx
@@ -1,38 +1,62 @@
-import { Button, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import React from "react";
 import { RootStackScreenProps } from "../types";
 
+const background = {
+  uri: "https://i.ibb.co/j3bbZdt/r-ticket-background.png",
+};
+
 const HomeScreen = ({ navigation }: RootStackScreenProps<"Home">) => {
   return (
-    <View style={styles.container}>
-      <Pressable
-        onPress={() => navigation.navigate("Select")}
-        style={styles.button}
-      >
-        <Text style={styles.text}>Appuyez pour continuer</Text>
-      </Pressable>
-    </View>
+    <ImageBackground
+      source={background}
+      resizeMode="stretch"
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Pressable
+          onPress={() => navigation.navigate("Select")}
+          style={styles.button}
+        >
+          <Text style={styles.text}>Appuyez pour continuer</Text>
+        </Pressable>
+      </View>
+    </ImageBackground>
   );
 };
 
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+  container: { flex: 1 },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+  },
   button: {
+    position: "absolute",
+    bottom: 160,
+    right: 260,
     alignItems: "center",
     justifyContent: "center",
-    borderRadius: 4,
+    borderRadius: 10,
     backgroundColor: "none",
-    borderWidth: 3,
-    height: 60,
-    width: 225,
+    borderWidth: 4,
+    borderColor: "#e4cf63",
+    height: 90,
+    width: 300,
   },
   text: {
-    fontSize: 16,
+    fontSize: 24,
     lineHeight: 21,
     fontWeight: "bold",
     letterSpacing: 0.25,
-    color: "black",
+    color: "#e4cf63",
   },
 });
