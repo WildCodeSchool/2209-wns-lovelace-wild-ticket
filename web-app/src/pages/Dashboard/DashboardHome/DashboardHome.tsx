4adfc61e536992e36f25eb164d25d0ff82140cbb
Estelle <estelle9.dev@gmail.com>
2023-03-29 15:53:26 +0200

    test

--- web-app/src/pages/Dashboard/DashboardHome/DashboardHome.tsx
+++ web-app/src/pages/Dashboard/DashboardHome/DashboardHome.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "../DashboardTemp.scss";
 import { AppContext } from "../../../context/AppContext";
 import SVGLogo from "../../../components/SVG/SVGLogo/SVGLogo";
@@ -6,15 +6,17 @@ import { BIG_LOGO_DASHBOARD_SIZE } from "../../../constants/Constants";
 
 const DashboardHome = () => {
   const appContext = useContext(AppContext);
+  const [dateNow, setDateNow] = useState<{ date: string; time: string }>();
 
   return (
     <div className="DashboardMain">
       <div className="DashboardContent">
-        <SVGLogo
+        {/* <SVGLogo
           logoWidth={BIG_LOGO_DASHBOARD_SIZE}
           logoHeight={BIG_LOGO_DASHBOARD_SIZE}
           logoFill={appContext?.userSVGColorScheme}
-        />
+        /> */}
+        <p className="HeaderTime">zhgekhrmohrthjtmoujy</p>
         <h1>DASHBOARD HOME</h1>
         <p className="DashboardText">Page Under Construction...</p>
         <p>Connecté avec l'adresse email : {appContext?.userData.email}</p>
