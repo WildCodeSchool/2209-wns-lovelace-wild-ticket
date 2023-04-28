fb530d4f27f670f31ba8148f391a40f9301148d9
Anthony Gouton <gouton.anthony@live.fr>
2023-04-28 14:46:54 +0200

    Ajout des filtres sur le tableau des poles

--- web-app/src/components/Dashboard/DashboardPoleListTab/DashboardPoleListTab.tsx
+++ web-app/src/components/Dashboard/DashboardPoleListTab/DashboardPoleListTab.tsx
@@ -1,8 +1,10 @@
-import { DataTable } from "primereact/datatable";
+import { DataTable, DataTableFilterMeta } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { GET_POLE_TYPES } from "../../../types/DataTypes";
 import SVGIconEdit from "../../SVG/SVGIconEdit/SVGIconEdit";
 import SVGIconDelete from "../../SVG/SVGIconDelete/SVGIconDelete";
+import { useState } from "react";
+import { FilterMatchMode } from "primereact/api";
 
 export default function DashboardPoleListTab({
   poles,
@@ -34,6 +36,14 @@ export default function DashboardPoleListTab({
     );
   };
 
+  const [filters] = useState<DataTableFilterMeta>({
+    name: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    address: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    zipCode: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    city: { value: null, matchMode: FilterMatchMode.CONTAINS },
+    email: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  });
+
   return (
     <div className="card" style={{ width: "100%" }}>
       <DataTable
@@ -44,15 +54,48 @@ export default function DashboardPoleListTab({
         paginator
         rows={10}
         rowsPerPageOptions={[5, 10, 20, 50]}
+        dataKey="id"
+        filters={filters}
+        filterDisplay="row"
       >
-        <Column field="name" header="Nom"></Column>
-        <Column field="address" header="Adresse"></Column>
-        <Column field="zipCode" header="Code Postal"></Column>
-        <Column field="city" header="Ville"></Column>
-        <Column field="email" header="Email"></Column>
+        <Column
+          field="name"
+          header="Nom"
+          filter
+          filterPlaceholder="Nom"
+          style={{ minWidth: "10rem" }}
+        ></Column>
+        <Column
+          field="address"
+          header="Adresse"
+          filter
+          filterPlaceholder="Adresse"
+          style={{ minWidth: "10rem" }}
+        ></Column>
+        <Column
+          field="zipCode"
+          header="Code Postal"
+          filter
+          filterPlaceholder="Code Postal"
+          style={{ minWidth: "10rem" }}
+        ></Column>
+        <Column
+          field="city"
+          header="Ville"
+          filter
+          filterPlaceholder="Ville"
+          style={{ minWidth: "10rem" }}
+        ></Column>
+        <Column
+          field="email"
+          header="Email"
+          filter
+          filterPlaceholder="Email"
+          style={{ minWidth: "10rem" }}
+        ></Column>
         <Column
           header="Actions"
-          style={{ flex: "0 0 4rem" }}
+          style={{ flex: "0 0 4rem", minWidth: "10rem" }}
           body={(pole: GET_POLE_TYPES) => actionButton(pole)}
         ></Column>
       </DataTable>
